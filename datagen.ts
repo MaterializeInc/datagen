b5d7fced7fab8cb9e5fac0b2a2b7ae3111594d42
Chuck Larrieu Casias <87353672+chuck-alt-delete@users.noreply.github.com>
2023-03-10 16:23:16 -0800

    Update datagen.ts

--- datagen.ts
+++ datagen.ts
@@ -17,7 +17,7 @@ import dataGenerator from './src/dataGenerator.js';
 import fs from 'fs';
 import { program, Option } from 'commander';
 
-program.name('datagen').description('Fake Data Generator').version('0.1.3');
+program.name('datagen').description('Fake Data Generator').version('0.1.4');
 
 program
     .requiredOption('-s, --schema <char>', 'Schema file to use')
