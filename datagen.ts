1a6ffc22ac71cd12840ff8401b3555eaf8e22912
Bobby Iliev <bobby@bobbyiliev.com>
2023-04-13 17:03:04 +0300

    Update version to v0.2.1 (#94)

--- datagen.ts
+++ datagen.ts
@@ -17,7 +17,7 @@ import dataGenerator from './src/dataGenerator.js';
 import fs from 'fs';
 import { program, Option } from 'commander';
 
-program.name('datagen').description('Fake Data Generator').version('0.2.0');
+program.name('datagen').description('Fake Data Generator').version('0.2.1');
 
 program
     .requiredOption('-s, --schema <char>', 'Schema file to use')
