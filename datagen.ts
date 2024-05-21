b5fa03f2e257048d05a701d4594d642e502e60ac
chuck-alt-delete <chuck@materialize.com>
2024-05-21 12:42:01 -0700

    make it a minor version bump rather than patch

--- datagen.ts
+++ datagen.ts
@@ -17,7 +17,7 @@ import dataGenerator from './src/dataGenerator.js';
 import fs from 'fs';
 import { program, Option } from 'commander';
 
-program.name('datagen').description('Fake Data Generator').version('0.5.1');
+program.name('datagen').description('Fake Data Generator').version('0.6.0');
 
 program
     .requiredOption('-s, --schema <char>', 'Schema file to use')
